a8f7b76cb0c846c02714e1eae945705b1b280b2b
Sabin Subedi <sabinsubedi125@gmail.com>
2020-08-26 14:21:33 +0545

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import React from "react";
 import { Navbar, NavbarBrand } from "reactstrap";
-import "./App.css";
 import Menu from "./components/MenuComponent";
+import "./App.css";
+import DISHES from "./shared/dishes";
 
-function App() {
-  return (
-    <div className='App'>
-      <Navbar dark color='primary'>
-        <div className='container'>
-          <NavbarBrand href='#'>Sabin Subedi</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dishes: DISHES,
+    };
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <Navbar dark color='primary'>
+          <div className='container'>
+            <NavbarBrand href='#'>Sabin Subedi</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes} />
+      </div>
+    );
+  }
 }
 
 export default App;
